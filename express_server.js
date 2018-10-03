e97e7e4f548d80f262142346bc4346a5dbb81de7
Angela Larryant <angelarryant@gmail.com>
2018-10-03 00:34:59 +0000

    generated random string and added key-value pair

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +11,8 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,6 +23,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
@@ -26,16 +35,50 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase.b2xVn2);
-  });
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase.b2xVn2);
+//   });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post("/urls", (req, res) => {
+  var shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  // console.log(urlDatabase);  // debug statement to see POST parameters
+  res.send(`http://localhost:8080/urls/${shortURL}`);
+  // console.log(urlDatabase);      // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = // let longURL = ...
+  res.redirect(longURL);
+});
+
+function generateRandomString() {
+var randomString = '';
+var possibleChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+// console.log(possibleChars.length);
+  for (let i = 0; i < 6; i++) {
+  randomString += possibleChars[getRandomInt(0, 62)];
+  };
+  return randomString;
+}
+
+// MDN code
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+
+// console.log(generateRandomString());
+
 
